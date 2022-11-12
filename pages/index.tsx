d203cc66ec4b43a2776216416d2ad34fef9e4c4d
Tsunfire <underrated@gmail.com>
2022-11-12 04:52:06 -0800

    try styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,36 +14,36 @@ export default function Home() {
     <Main>
       <Article>
         {/* <div className="absolute h-screen w-full"></div> */}
-        <div className="background-gradient absolute h-full w-[100vw]">
+        <div className="background-gradient absolute h-full w-[100vw] opacity-20">
           <div className="background-gradient-pattern" />
         </div>
         <div className="relative flex h-screen w-full items-start justify-center pt-48">
           <div className="relative flex w-full flex-col items-start px-4 py-4">
             <div className="flex w-full flex-row space-x-4">
               <Tabs.Root className="w-full">
-                <Tabs.List className="flex flex-row">
+                <Tabs.List className="relative z-10 flex flex-row">
                   <Tabs.Trigger
                     value="tab1"
-                    className="cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-12 py-2 transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
                     <p className="">dAMM</p>
                   </Tabs.Trigger>
                   <Tabs.Trigger
                     value="tab2"
-                    className="cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-12 py-2 transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
                     <p className="">Fuji AMM</p>
                   </Tabs.Trigger>
                   <Tabs.Trigger
                     value="tab3"
-                    className="cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-12 py-2 transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
                     <p className="">Arbitrum AMM</p>
                   </Tabs.Trigger>
                 </Tabs.List>
                 <Tabs.Content value="tab1">
                   <div className="bg relative flex w-full flex-col space-y-1 rounded-sm rounded-tl-none p-8 shadow-damn">
-                    <div className="background-gradient pointer-events-none absolute h-full w-[100vw]">
+                    <div className="background-gradient pointer-events-none absolute h-full w-[36rem] opacity-20">
                       <div className="background-gradient-pattern" />
                     </div>
                     <Tabs.Root
@@ -58,7 +58,7 @@ export default function Home() {
                         >
                           <p
                             className={`font-light ${
-                              activeTab === "tab1" && "drop-shadow-soju"
+                              activeTab === "tab1" && ""
                             }`}
                           >
                             Provide
@@ -70,7 +70,7 @@ export default function Home() {
                         >
                           <p
                             className={`font-light ${
-                              activeTab === "tab2" && "drop-shadow-soju"
+                              activeTab === "tab2" && ""
                             }`}
                           >
                             Withdraw
@@ -82,7 +82,7 @@ export default function Home() {
                         >
                           <p
                             className={`font-light ${
-                              activeTab === "tab3" && "drop-shadow-soju"
+                              activeTab === "tab3" && ""
                             }`}
                           >
                             Stats
@@ -95,9 +95,9 @@ export default function Home() {
                             className="bg-transparent font-wagmi text-xl text-white  placeholder:text-white/50 focus:outline-none"
                             placeholder="0.00"
                           />
-                          <p className="h-fit rounded-sm border border-sky-400 bg-sky-400/10 px-2 py-0.5 text-sky-400 ">
+                          <h4 className="h-fit rounded-sm border border-white/5 px-2 py-0.5 text-white/50 ">
                             EXP
-                          </p>
+                          </h4>
                         </div>
                         <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm border border-white/5 p-4">
                           <input
@@ -105,9 +105,9 @@ export default function Home() {
                             placeholder="0.00"
                           />
 
-                          <p className="h-fit rounded-sm border border-white bg-white/10 px-2 py-0.5 text-white ">
-                            ETH
-                          </p>
+                          <h4 className="h-fit rounded-sm border border-white/5 px-2 py-0.5 text-white/50 ">
+                            SOJU
+                          </h4>
                         </div>
                         <div className="flex h-16 w-full items-center justify-center rounded-sm border border-white/5 shadow-damn">
                           <p className="text-white drop-shadow-soju">
@@ -116,54 +116,66 @@ export default function Home() {
                         </div>
                       </Tabs.Content>
                       <Tabs.Content value="tab2">
-                        <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm bg-white/5 p-4">
+                        <h3 className="mb-2 text-white">39.93</h3>
+                        <p className="mb-4 font-thin uppercase tracking-widest text-white/50">
+                          Total balance (ETH)
+                        </p>
+                        <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm border border-white/5 p-4">
                           <input
                             className="bg-transparent font-wagmi text-xl text-white  placeholder:text-white/50 focus:outline-none"
                             placeholder="0.00"
                           />
-
-                          <p className="h-fit rounded-sm border border-primary bg-primary/10 px-2 py-0.5 text-primary ">
-                            ETH
-                          </p>
+                          <h4 className="h-fit rounded-sm border border-white/5 px-2 py-0.5 text-white/50 ">
+                            EXP
+                          </h4>
                         </div>
-                        <div className="flex h-16 w-full items-center justify-center rounded-sm bg-primary">
-                          <p className="text-secondary">Withdraw</p>
+                        <div className="flex h-16 w-full items-center justify-center rounded-sm border border-white/5 shadow-damn">
+                          <p className="text-white drop-shadow-soju">
+                            Withdraw
+                          </p>
                         </div>
                       </Tabs.Content>
                       <Tabs.Content value="tab3">
-                        <p className="text-white">Stats</p>
+                        <h3 className="mb-2 text-white">139.14</h3>
+                        <p className="mb-4 font-thin uppercase tracking-widest text-white/50">
+                          Reserve 1 (EXP)
+                        </p>
+                        <div className="mb-4 h-px w-full bg-white/5" />
+                        <h3 className="mb-2 text-white">23.64</h3>
+                        <p className="mb-4 font-thin uppercase tracking-widest text-white/50">
+                          Reserve 1 (SOJU)
+                        </p>
                       </Tabs.Content>
                     </Tabs.Root>
                   </div>
                 </Tabs.Content>
                 <Tabs.Content value="tab2">
                   <div className="bg flex w-full flex-col rounded-sm p-8 shadow-damn">
-                    <h3 className="mb-4 text-white">Swap</h3>
-                    <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm bg-white/5 p-4">
+                    <div className="background-gradient pointer-events-none absolute h-full w-[36rem] opacity-20">
+                      <div className="background-gradient-pattern" />
+                    </div>
+                    <p className="mb-4 font-thin text-white">Swap</p>
+                    <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm border border-white/5 p-4">
                       <input
                         className="bg-transparent font-wagmi text-xl text-white  placeholder:text-white/50 focus:outline-none"
                         placeholder="0.00"
                       />
-                      <p className="h-fit rounded-sm border border-white bg-white/10 px-2 py-0.5 text-white">
+                      <h4 className="h-fit rounded-sm border border-white/5 px-2 py-0.5 text-white/50 ">
                         EXP
-                      </p>
+                      </h4>
                     </div>
-                    <div className="mb-4 flex h-20 w-full items-start justify-between rounded-sm bg-white/5 p-4">
+                    <div className="relative mb-4 flex h-20 w-full items-start justify-between rounded-sm border border-white/5 p-4">
                       <input
                         className="bg-transparent font-wagmi text-xl text-white  placeholder:text-white/50 focus:outline-none"
                         placeholder="0.00"
                       />
-
-                      <p className="h-fit rounded-sm border border-white bg-white/10 px-2 py-0.5 text-white ">
-                        ETH
-                      </p>
+                      <h4 className="h-fit rounded-sm border border-white/5 px-2 py-0.5 text-white/50 ">
+                        EXP
+                      </h4>
                     </div>
-                    <div className="flex h-16 w-full items-center justify-center rounded-sm bg-primary">
-                      <p className="text-secondary">Swap</p>
+                    <div className="flex h-16 w-full items-center justify-center rounded-sm border border-white/5 shadow-damn">
+                      <p className="text-white drop-shadow-soju">Swap</p>
                     </div>
-                    <button className="glow-button">
-                      <span>Button</span>
-                    </button>
                   </div>
                 </Tabs.Content>
                 <Tabs.Content value="tab3"></Tabs.Content>
