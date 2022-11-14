ff9f70a2e41c17189f8d660285e03424f319fa42
Tsunfire <underrated@gmail.com>
2022-11-14 01:23:45 -0800

    dove supremacy

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import SwapTabContent from "../components/SwapTabContent";
 import DammTabContent from "../components/DammTabContent";
 import SettingsTabContent from "../components/SettingsTabContent";
 import { useStore } from "../lib/store";
+import { GiPeaceDove } from "react-icons/gi";
 
 export default function Home() {
   const isAutoSwitch = useStore((state) => state.isAutoSwitch);
@@ -56,20 +57,29 @@ export default function Home() {
                 <Tabs.List className="relative z-10 flex flex-row">
                   <Tabs.Trigger
                     value="damm"
-                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="relative w-full cursor-pointer overflow-hidden rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
+                    {activeNetworkTab === "damm" && (
+                      <GiPeaceDove className="absolute right-0 -rotate-45 text-6xl text-white/5" />
+                    )}
                     <p className="font-normal">dAMM</p>
                   </Tabs.Trigger>
                   <Tabs.Trigger
                     value="fuji"
-                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="relative w-full cursor-pointer overflow-hidden rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
+                    {activeNetworkTab === "fuji" && (
+                      <GiPeaceDove className="absolute right-0 -rotate-45 text-6xl text-white/5" />
+                    )}
                     <p className="font-normal">Fuji AMM</p>
                   </Tabs.Trigger>
                   <Tabs.Trigger
                     value="arbi"
-                    className="w-full cursor-pointer rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
+                    className="relative w-full cursor-pointer overflow-hidden rounded-sm rounded-b-none border border-b-0 border-white/5 bg-black/10 px-4 py-2 text-left transition duration-300 ease-linear hover:text-white focus:outline-none rdx-state-active:bg-[#313135] rdx-state-active:text-white rdx-state-inactive:text-white/50"
                   >
+                    {activeNetworkTab === "arbi" && (
+                      <GiPeaceDove className="absolute right-0 -rotate-45 text-6xl text-white/5" />
+                    )}
                     <p className="font-normal">Arbitrum AMM</p>
                   </Tabs.Trigger>
                   <Tabs.Trigger
