be5d2a1e8bad0a4d458e5db92d7a30623c24bab5
soju <underrated@gmail.com>
2023-03-26 03:43:31 -0700

    syncing working

--- lib/hooks/sync/useSyncL2.ts
+++ lib/hooks/sync/useSyncL2.ts
@@ -1,26 +1,29 @@
 import { ethers } from "ethers";
 import { useMemo } from "react";
-import { ChainId, DOVE_ADDRESS, LZ_CHAIN, PAIR_ADDRESS } from "../../../sdk";
+import { ChainId, DOVE_ADDRESS, HL_DOMAIN, PAIR_ADDRESS } from "../../../sdk";
 import { useDoveSyncL2, usePrepareDoveSyncL2 } from "../../../src/generated";
 import { wrapAddress } from "../../utils/wrapAddress";
 
 export default function useSyncL2(chainToSync: ChainId): {
   sync: () => void;
 } {
-  const lzChainId = useMemo(() => {
+  const domainId = useMemo(() => {
     if (!chainToSync) return;
 
-    return LZ_CHAIN[chainToSync];
+    return HL_DOMAIN[chainToSync];
   }, [chainToSync]);
 
   const { config } = usePrepareDoveSyncL2({
     address: DOVE_ADDRESS[ChainId.ETHEREUM_GOERLI] as `0x${string}`,
-    args: [lzChainId!, wrapAddress(PAIR_ADDRESS[chainToSync])],
+    args: [domainId ?? 0, wrapAddress(PAIR_ADDRESS[chainToSync])],
     overrides: {
-      value: ethers.utils.parseEther("0.2"),
+      // TODO: estimate gas with SDK or onchain
+      value: ethers.utils.parseEther("0.5"),
     },
-    enabled: !!lzChainId,
+    enabled: !!domainId,
   });
+
+  console.log(domainId, PAIR_ADDRESS[chainToSync]);
 
   const { write } = useDoveSyncL2(config);
 
--- sdk/constants/lzChains.ts
+++ sdk/constants/lzChains.ts
@@ -6,3 +6,10 @@ export const LZ_CHAIN: { [chainId in ChainId]: number } = {
   [ChainId.POLYGON_MUMBAI]: 10109,
   [ChainId.AVALANCHE_FUJI]: 10106,
 };
+
+export const HL_DOMAIN: { [chainId in ChainId]: number } = {
+  [ChainId.ETHEREUM_GOERLI]: 5,
+  [ChainId.ARBITRUM_GOERLI]: 421613,
+  [ChainId.POLYGON_MUMBAI]: 80001,
+  [ChainId.AVALANCHE_FUJI]: 43113,
+}
