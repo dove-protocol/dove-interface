f4ebf55ca578bacdd5e9c69b70abe7c6b1aae0ef
Tsunfire <underrated@gmail.com>
2023-05-02 20:49:00 -0700

    feat: add og image seo

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -1,7 +1,6 @@
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { NextSeo } from "next-seo";
-import Navbar from "../Navbar";
 
 const Main = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
@@ -14,14 +13,21 @@ const Main = ({ children }: { children: React.ReactNode }) => {
         <title>Dove Interface</title>
       </Head>
       <NextSeo
-        title="Dove Interface"
-        description="Dove Interface"
+        useAppDir
+        title="Dove"
+        description="Experience the power of Dove, our innovative solution that eliminates liquidity fragmentation and bridges gaps across Layer 2 networks. Focus on the technical merits of your chosen L2, while Dove unlocks boundless liquidity for executing advanced leveraged LSD strategies on any L2."
         openGraph={{
-          url: "",
-          title: "Wagmi",
-          description: "Wagmi",
+          type: "website",
+          description:
+            "Experience the power of Dove, our innovative solution that eliminates liquidity fragmentation and bridges gaps across Layer 2 networks. Focus on the technical merits of your chosen L2, while Dove unlocks boundless liquidity for executing advanced leveraged LSD strategies on any L2.",
+          title: "Dove",
+          images: [
+            {
+              url: "https://i.imgur.com/6fGh6n4.png",
+            },
+          ],
         }}
-        twitter={{ handle: "@wagmi" }}
+        twitter={{ cardType: "summary_large_image", handle: "@doveprotocol" }}
       />
       {children}
     </div>
