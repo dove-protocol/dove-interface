e87684e955e3298be522b0ea8e2bedc616445d39
Tsunfire <underrated@gmail.com>
2023-05-02 21:10:33 -0700

    Update Main.tsx

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -1,21 +1,10 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 const Main = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const path = router.asPath;
 
-  return (
-    <div className="mx-auto">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Dove Interface</title>
-     
-      </Head>
-
-      {children}
-    </div>
-  );
+  return <div className="mx-auto">{children}</div>;
 };
 
 export default Main;
